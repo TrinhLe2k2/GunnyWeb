25de778e52997729b7a1fed0f94b5f1a0c4a5404
Khánh Trình <khanhtrinh.phuyen@gmail.com>
2025-12-29 22:02:40 +0700

    Add project files.

---
+++ GunnyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GunnyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
